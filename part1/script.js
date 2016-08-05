ddb7dd174ebb5edd4f64712dc8cdbbe12b6160e1
Yuliya Ryadskaya <juliya.vasilisa@gmail.com>
2016-08-05 22:21:25 +0300

    First commit 05.08.16

---
+++ part1/script.js
@@ -0,0 +1,15 @@
+﻿function pow(x, n) {
+var result = x;
+for(var i = 1; i < n; i++){
+result *= x;
+}
+return result;
+}
+var x = prompt("Введите число x", '');
+var n = prompt("Введите целую степень n > 1", '');
+
+if (n <= 1) {
+	alert('Степень '+ n + 'не поддерживается, введите целую степень, большую 1');
+} else {
+	console.log(pow(x,n));
+}
