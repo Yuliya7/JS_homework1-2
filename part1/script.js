0aa26682bc473dc0987d3b6d2afc7ab5e0c17d42
Yuliya Ryadskaya <juliya.vasilisa@gmail.com>
2016-08-11 21:07:00 +0300

    after checking 11.08.2016

--- part1/script.js
+++ part1/script.js
@@ -1,15 +1,16 @@
-﻿function pow(x, n) {
-var result = x;
-for(var i = 1; i < n; i++){
-result *= x;
-}
-return result;
-}
-var x = prompt("Введите число x", '');
-var n = prompt("Введите целую степень n > 1", '');
+﻿var x = prompt('Введите число x');
+var n = prompt('Введите степень n');
 
-if (n <= 1) {
-	alert('Степень '+ n + 'не поддерживается, введите целую степень, большую 1');
-} else {
-	console.log(pow(x,n));
+function pow(x, n) {
+    var result = 1;
+    for (var i = 0; i < Math.abs(n); i++){
+        if (n < 0){
+                result /= x;
+        } else {
+            result *= x;
+        }
+    }
+  return result;
 }
+var result = pow(x, n);
+console.log ('Результат вычислений= ', result); 
